f8b734c77d8e28be96f89ffe5d8e3145b67b8cec
Breck <breck7@gmail.com>
2012-12-13 21:41:55 -0800

    Added duckduckgo

---
+++ companies/duckduckgo.js
@@ -0,0 +1,18 @@
+var request = require('request'),
+    Note = require('note'),
+    noteapi = require('../noteapi')
+
+var duckduckgo = {}
+
+duckduckgo.search = function (note, callback) {
+  note.q = note.query
+  delete note.query
+  note.format = 'json'
+  var get = noteapi.noteToGet(note)
+  request.get('http://api.duckduckgo.com/?' + get, function (error, response, body) {
+    if (error) return callback(error)
+    callback(false, new Note(JSON.parse(body)))
+  })
+}
+
+module.exports = duckduckgo
